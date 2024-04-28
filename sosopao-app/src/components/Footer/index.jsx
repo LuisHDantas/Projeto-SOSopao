93e6a7a25008d1aa3fc6677bb9df565e5b50d925
personal <lhgdantas@gmail.com>
2024-04-28 20:14:07 -0300

    footer 1.0

--- sosopao-app/src/components/Footer/index.jsx
+++ sosopao-app/src/components/Footer/index.jsx
@@ -12,45 +12,48 @@ import { IconContext } from "react-icons";
 export function Footer(){
     return(
         <footer>
-            <h3>Contato</h3>
+            <h2>Contato</h2>
 
             <div className='contato'>
-                <div className='contato-container'>
-                    <IconContext.Provider value={{ color: "#038C8C", className: "global-class-name" }}>
-                        <div>
-                            <BsTelephoneFill /> 
-                        </div>
-                    </IconContext.Provider>
-                    <p>{contatos.TELEFONE}</p>
-                </div>
+                <div id="informacoes-contato">
+                    <div className='contato-container'>
+                        <IconContext.Provider value={{ color: "#038C8C", className: "global-class-name" }}>
+                            <div>
+                                <BsTelephoneFill /> 
+                            </div>
+                        </IconContext.Provider>
+                        <p>{contatos.TELEFONE}</p>
+                    </div>
 
-                <div className='contato-container'>
-                    <IconContext.Provider value={{ color: "#038C8C", className: "global-class-name" }}>
-                        <div>
-                            <MdAlternateEmail />
-                        </div>
-                    </IconContext.Provider>
-                    <p>{contatos.EMAIL}</p>
-                </div>
+                    <div className='contato-container'>
+                        <IconContext.Provider value={{ color: "#038C8C", className: "global-class-name" }}>
+                            <div>
+                                <MdAlternateEmail />
+                            </div>
+                        </IconContext.Provider>
+                        <p>{contatos.EMAIL}</p>
+                    </div>
 
-                <div className='contato-container'>
-                    <IconContext.Provider value={{ color: "#038C8C", className: "global-class-name" }}>
-                        <div>
-                            <IoLocationSharp />
-                        </div>
-                    </IconContext.Provider>
-                    <p>{contatos.ENDERECO}</p>
-                </div>
+                    <div className='contato-container'>
+                        <IconContext.Provider value={{ color: "#038C8C", className: "global-class-name" }}>
+                            <div>
+                                <IoLocationSharp />
+                            </div>
+                        </IconContext.Provider>
+                        <p>{contatos.ENDERECO}</p>
+                    </div>
+                    
                 
-            
-                <div className='contato-container'>
-                    <IconContext.Provider value={{ color: "#038C8C", className: "global-class-name" }}>
-                        <div>
-                            <IoIosGlobe />
-                        </div>
-                    </IconContext.Provider>
-                    <p>{contatos.SITE}</p>
+                    <div className='contato-container'>
+                        <IconContext.Provider value={{ color: "#038C8C", className: "global-class-name" }}>
+                            <div>
+                                <IoIosGlobe />
+                            </div>
+                        </IconContext.Provider>
+                        <p>{contatos.SITE}</p>
+                    </div>
                 </div>
+
 
                 <div className='redes-sociais'>
                     <IconContext.Provider value={{ color: "#038C8C", size: "2em", className: "global-class-name" }}>
