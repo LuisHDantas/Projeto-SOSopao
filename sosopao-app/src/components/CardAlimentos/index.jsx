6f1d5fec70766ef814da6baa8ce4f14911e02fa6
Gabriel Almeida <gabrielsousa230@gmail.com>
2024-04-30 19:31:22 -0300

    Mobile itenzinho sem botão

--- sosopao-app/src/components/CardAlimentos/index.jsx
+++ sosopao-app/src/components/CardAlimentos/index.jsx
@@ -1,40 +1,54 @@
+import { useState } from 'react';
+import { ButtonAdd } from '../buttonAdd';
+import { CardItemAlimento } from '../CardItemAlimento';
 import './style.css'
 
 
 export function CardAlimentos(){
+    const [isOpen, setIsOpen] = useState(false);
+    
     return(
-        <div className="card-alimentos">
-            <div className="foto-btns-alimentos">
-                <img src="https://avatars.githubusercontent.com/u/45525241?v=4" alt="foto de perfil" />
-            </div>
-            
-            <div className='infos-alimentos'>
-                <div className="sub-info-alimentos">
-                    <div className="info-alimentos">
-                        <h4>Nome:</h4>
-                        <p>Macarrão</p>
-                    </div>
-
-                    <div className="info-alimentos">
-                        <h4>Meta:</h4>
-                        <p>20</p>
-                    </div>
+        <>
+            <div className="card-alimentos" onClick={() => setIsOpen(!isOpen)}>
+                <div className="foto-btns-alimentos">
+                    <img src="https://avatars.githubusercontent.com/u/45525241?v=4" alt="foto de perfil" />
                 </div>
                 
+                <div className='infos-alimentos'>
+                    <div className="sub-info-alimentos">
+                        <div className="info-alimentos">
+                            <h4>Nome:</h4>
+                            <p>Macarrão</p>
+                        </div>
 
-                <div className="sub-info-alimentos">
-                    <div className="info-alimentos">
-                        <h4>Validade:</h4>
-                        <p>XX/XX/XXXX</p>
+                        <div className="info-alimentos">
+                            <h4>Meta:</h4>
+                            <p>20</p>
+                        </div>
                     </div>
-                    <div className="info-alimentos">
-                        <h4>Qtd Atual:</h4>
-                        <p>10</p>
+                    
+
+                    <div className="sub-info-alimentos">
+                        <div className="info-alimentos">
+                            <h4>Validade:</h4>
+                            <p>XX/XX/XXXX</p>
+                        </div>
+                        <div className="info-alimentos">
+                            <h4>Qtd Atual:</h4>
+                            <p>10</p>
+                        </div>
                     </div>
                 </div>
             </div>
             
-            
-        </div>
+            {
+                isOpen && 
+                <div className='new-container-alimentos'>
+                    <button className='button-add-itenzinho'>+ Adicionar Item</button>
+                    <CardItemAlimento/>
+                </div>
+            }
+
+        </>
     );
 }
---
+++ sosopao-app/src/components/CardItemAlimento/index.jsx
@@ -0,0 +1,35 @@
+import './style.css'
+
+export function CardItemAlimento(){
+
+    return(
+        <div className="card-item-alimento">
+            <div className="card-item-alimento-info">
+                <div className="card-marca-data">
+                    <div className="item-alimento">
+                        <h4>Marca</h4>
+                        <p>adria</p>
+                    </div>
+                    <div className="item-alimento">
+                        <h4>Data</h4>
+                        <p>XX/XX/XXXX</p>
+                    </div>
+                </div>
+
+                <div className="card-validade-uniMed">
+                    <div className="item-alimento">
+                        <h4>Validade</h4>
+                        <p>XX/XX/XXXX</p>
+                    </div>
+                    <div className="item-alimento">
+                        <h4>Un. Med</h4>
+                        <p>Unidade</p>
+                    </div>
+                </div>
+                
+            </div>
+           
+           <button>oi</button>
+        </div>
+    );
+}
