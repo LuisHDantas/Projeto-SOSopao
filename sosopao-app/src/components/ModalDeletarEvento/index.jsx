16fa78e62343fba073748bd391ab1384cc754e40
personal <lhgdantas@gmail.com>
2024-04-30 10:56:32 -0300

    fix: icone botao deletar

--- sosopao-app/src/components/ModalDeletarEvento/index.jsx
+++ sosopao-app/src/components/ModalDeletarEvento/index.jsx
@@ -1,11 +1,13 @@
 import './style.css';
+import { TbLetterX } from "react-icons/tb";
+
 
 export function DeletarEvento({fechaDeletar}){
     return(
         <div className="deletar-evento-modal">
             <p>Deseja EXCLUIR esse evento?</p>
             <div className='btns-modal-deletar'>
-                <button id='btn-excluir-evento'>Excluir</button>
+                <button id='btn-excluir-evento'> <TbLetterX className='icon-excluir-evento'/> Excluir</button>
                 <button id='btn-cancelar-excluir-evento' onClick={fechaDeletar}>Cancelar</button>
             </div>
             
