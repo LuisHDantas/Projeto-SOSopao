5ac26f431ae4b4b5c4e602d20973dc995e59dd1d
Lucas Piovani <lucaspiovani@usp.br>
2024-05-01 16:43:07 -0300

    Fix: Contatos do footer atualizados.

--- sosopao-app/src/constants.js
+++ sosopao-app/src/constants.js
@@ -1,8 +1,10 @@
 export const contatos = {
-    TELEFONE: '(16) 99777-8482',
-    EMAIL: 'sosopao@gmail.com',
+    TELEFONE: '(16) 99781-1010',
+    EMAIL: 'sosassociacao@gmail.com',
     CIDADE: 'São Carlos - SP',
-    ENDERECO: 'Rua Dona Maria Isabel de Oliveira',
+    ENDERECO: 'Rua São Joaquim 2037, Jd Macarengo',
+    WHATSAPP: 'https://wa.me/5516997811010',
+    INSTAGRAM: 'https://www.instagram.com/s.o.sopao/',
+    FACEBOOK: 'https://www.facebook.com/sosopaofraterno/'
 };
 
-  
