833fbeb25fb84f4c5651182156035f3115d8ee15
Gabriel Almeida <gabrielsousa230@gmail.com>
2024-05-02 21:50:35 -0300

    fix: ajustes finais de fonte

--- sosopao-app/src/pages/Home.jsx
+++ sosopao-app/src/pages/Home.jsx
@@ -5,27 +5,27 @@ import '../styles/home.css';
 export function Home(){
     return(
         <>
-            <body>
-                <Navbar type='home'/>
+            
+            <Navbar type='home'/>
 
-                <div id="tituloUsuario">
-                    <h1>Olá, *usuário*</h1>
-                </div>
+            <div id="tituloUsuario">
+                <h1>Olá, *usuário*</h1>
+            </div>
 
-                <div id="conteudoGuia">
-                    <p>Através da aba de navegação no canto superior direito, você pode acessar as seguintes páginas: </p>
+            <div id="conteudoGuia">
+                <p>Através da aba de navegação no canto superior direito, você pode acessar as seguintes páginas: </p>
 
-                    <ul>
-                        <li>Estoque de Alimentos: nessa página, é possível visualizar e gerir o estoque de alimentos, criando e editando categorias de alimentos e itens individuais de cada categoria.</li>
-                        <li>Estoque de Itens: nessa página, é possível visualizar e gerir o estoque de itens de vestuário e pessoais, criando e editando categorias de itens e itens individuais de cada categoria.</li>
-                        <li>Eventos: nessa página, é possivel visualizar e gerir os eventos ativos e já realizados, editando suas informações e imagens.</li>
-                        <li>Rotas: nessa página, é possivel visualizar os pontos da rota de distribuição, removê-los, reordená-los, ou adicionar um novo ponto à rota.</li>
-                        <li>Painel de Controle: nessa página, é possível alterar suas informações de usuário, criar e gerir a lista de usuários administradores, alterar as informações de contato disponíveis no site e alterar  o QR Code para doações via PIX.</li>
-                    </ul>
-                </div>
+                <ul>
+                    <li>Estoque de Alimentos: nessa página, é possível visualizar e gerir o estoque de alimentos, criando e editando categorias de alimentos e itens individuais de cada categoria.</li>
+                    <li>Estoque de Itens: nessa página, é possível visualizar e gerir o estoque de itens de vestuário e pessoais, criando e editando categorias de itens e itens individuais de cada categoria.</li>
+                    <li>Eventos: nessa página, é possivel visualizar e gerir os eventos ativos e já realizados, editando suas informações e imagens.</li>
+                    <li>Rotas: nessa página, é possivel visualizar os pontos da rota de distribuição, removê-los, reordená-los, ou adicionar um novo ponto à rota.</li>
+                    <li>Painel de Controle: nessa página, é possível alterar suas informações de usuário, criar e gerir a lista de usuários administradores, alterar as informações de contato disponíveis no site e alterar  o QR Code para doações via PIX.</li>
+                </ul>
+            </div>
 
-                <Footer/>
-            </body>
+            <Footer/>
+            
         </>
     );
 }
